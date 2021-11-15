565a2256058010913f81e94234bf3c71fc7ba0fd
Swapnil <90128544+Swapnil-Pradhan@users.noreply.github.com>
2021-11-15 18:34:55 +0530

    Add files via upload

---
+++ Update.js
@@ -0,0 +1,5 @@
+var update = document.getElementById("update")
+if(update.innerHTML<2){
+alert("New update available!\nUpdate Search It")
+window.open("https://swapnil-pradhan.github.io/Search-It/SearchIt.apk");
+}
