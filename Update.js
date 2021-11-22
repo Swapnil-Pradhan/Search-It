eaec4a315364b5163141090c10909f5014b06e97
Swapnil <90128544+Swapnil-Pradhan@users.noreply.github.com>
2021-11-22 20:58:12 +0530

    Delete Update.js

--- Update.js
+++
@@ -1,5 +0,0 @@
-var update = document.getElementById("update")
-if(update.innerHTML<2){
-alert("New update available!\nUpdate Search It")
-window.open("https://swapnil-pradhan.github.io/Search-It/SearchIt.apk");
-}
