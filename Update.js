99f856ee8a8ed7d7c777b3d29f70ee2427d931a8
Swapnil <90128544+Swapnil-Pradhan@users.noreply.github.com>
2021-11-23 12:45:23 +0530

    Delete Update.js

--- Update.js
+++
@@ -1,9 +0,0 @@
-var update = document.getElementById("update")
-if(update.innerHTML!=3.1){
-document.getElementById("updatebg").style.display="flex";
-document.getElementById("updatebg").style.opacity="1";
-document.getElementById("version").style.innerHTML+=")"
-document.getElementById("line1").innerHTML="";
-document.getElementById("line2").innerHTML="";
-document.getElementById("line3").innerHTML="";
-}
